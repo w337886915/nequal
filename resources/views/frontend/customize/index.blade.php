cf5dce43202f6aeeadcb0feca037ff3b8c1a3d5a
liweiiewil <418832101@qq.com>
2019-05-22 11:30:27 +0800

    首页样式修改

--- resources/views/frontend/customize/index.blade.php
+++ resources/views/frontend/customize/index.blade.php
@@ -1,1 +1,6 @@
 @extends('frontend.customize.layouts.app')
+@section('content')
+    <div style="min-height:500px;width: 100%;">
+
+    </div>
+@endsection
--- resources/views/frontend/customize/layouts/app.blade.php
+++ resources/views/frontend/customize/layouts/app.blade.php
@@ -8,36 +8,7 @@
     <title>Document</title>
     <link rel="stylesheet" href="https://cdn.bootcss.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/Swiper/4.0.2/css/swiper.min.css">
-    <style>
-        .container-fixed .container nav #navbarSupportedContent .navbar-nav{
-            line-height: 88px;
-            height: 88px;
-            text-align: center;
-        }
-        .container-fixed .container nav #navbarSupportedContent .navbar-nav li {
-            width: 100px;
-        }
-        .container-fixed .container nav #navbarSupportedContent .navbar-nav li a{
-            color: black;
-        }
-        .container-fixed .container nav #navbarSupportedContent .navbar-nav li a button{
-            border-radius:20px;
-        }
-    </style>
-    <style>
-        .swap {
-            position: relative;
-            margin: auto 0;
-            min-height: 500px;
-        }
-        .swap .case{position: absolute;top:10px;left:200px;text-align: center;position: absolute;top: -46px;left: 250px;text-align: center;z-index: 2;}
-        .swap .case .item1{width: 460px;height: 270px;background-color: red;float: left; margin-right:20px;background-image: url("http://image.whgjh.top/block_03.gif")}
-        .swap .case .item2{width: 460px;height: 270px;background-color: yellow;float: left; margin-right:20px;background-image: url("http://image.whgjh.top/block_05.gif")}
-        .swap .case .item3{width: 460px;height: 270px;background-color: blue;float: left; background-image: url("http://image.whgjh.top/block_07.gif")}
-    </style>
-    <style>
-        .footer{height: 500px;background-color: black}
-    </style>
+    <link rel="stylesheet" href="{{asset('css/common.css')}}">
 </head>
 <body>
 <!-- header -->
@@ -45,11 +16,11 @@
 <!-- banner -->
 @include('frontend.customize.layouts.swipper')
 <!--content-->
-<div class="container-fixed swap">
+<div class="container-fixed">
     @yield('content')
 </div>
 <!--footer-->
-@include('frontend.customize.layouts.footer');
+@include('frontend.customize.layouts.footer')
 
 </body>
 <script src="https://cdn.bootcss.com/jquery/3.2.1/jquery.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
